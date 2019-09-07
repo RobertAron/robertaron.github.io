fef062e3b2bc75fcfd9c2e30c9738aba2b68159e
unknown <Robert.Aron.tx@gmail.com>
2019-09-06 23:42:41 -0500

    Updates

---
+++ precache-manifest.bd6cb988d64c115392bbadee7ac104b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeecd5d643a6116fbc8f7e1e6302b5cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9fb89a3f91b8696ac4d",
+    "url": "/static/css/main.eb5b3bba.chunk.css"
+  },
+  {
+    "revision": "9ddc71cb26dcb4fa62f2",
+    "url": "/static/js/2.ba1c8563.chunk.js"
+  },
+  {
+    "revision": "b9fb89a3f91b8696ac4d",
+    "url": "/static/js/main.f2216ada.chunk.js"
+  },
+  {
+    "revision": "cac7a6a49329c2134334",
+    "url": "/static/js/runtime~main.7791fe9c.js"
+  },
+  {
+    "revision": "1c0289e9e85e8d82e73297fb9ce9a61b",
+    "url": "/static/media/Base-Logo.1c0289e9.png"
+  },
+  {
+    "revision": "7bc5f3a1447cb3f129a83f93d45b1974",
+    "url": "/static/media/Blizzard-Logo.7bc5f3a1.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.54a6bd164d9e5fd8682efd140130d708.js"
+  "/precache-manifest.bd6cb988d64c115392bbadee7ac104b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
